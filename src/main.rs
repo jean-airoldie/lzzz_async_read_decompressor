ddddd4e65e3c54644ddaff1de4078665e14d258a
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2020-07-28 03:17:54 +0000

    Added rng bytes

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
-use {futures_lite::*, lzzzz::lz4f, piper::pipe, smol::Task};
+use {
+    bytes::Bytes,
+    futures_lite::*,
+    lazy_static::lazy_static,
+    lzzzz::lz4f,
+    piper::pipe,
+    rand::{distributions::Standard, rngs::SmallRng, Rng, SeedableRng},
+    smol::Task,
+};
 
-const FRAME_SIZE: usize = 8192;
+const FRAME_SIZE: usize = 1024;
+
+lazy_static! {
+    static ref FRAME: Bytes = {
+        let rng = SmallRng::seed_from_u64(42060);
+        rng.sample_iter(Standard).take(FRAME_SIZE).collect()
+    };
+}
 
 fn main() {
     smol::run(async {
-        let (reader, writer) = pipe(53);
+        let (reader, writer) = pipe(54);
 
-        let frame = vec![1u8; FRAME_SIZE];
-
-        let frame2 = frame.clone();
         let task = Task::spawn(async move {
             let mut reader = lz4f::AsyncReadDecompressor::new(reader).unwrap();
             let mut buf = vec![];
             reader.read_to_end(&mut buf).await.unwrap();
-            assert_eq!(buf, frame2);
+            assert_eq!(buf, *FRAME);
         });
 
         let mut writer =
             lz4f::AsyncWriteCompressor::new(writer, lz4f::Preferences::default()).unwrap();
-        writer.write_all(frame.as_ref()).await.unwrap();
+        writer.write_all(FRAME.as_ref()).await.unwrap();
         writer.flush().await.unwrap();
 
         // Signal EOF for the writer.
